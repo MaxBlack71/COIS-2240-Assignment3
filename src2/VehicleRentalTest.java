06fe79f257a0a04f3dbcb963bcf534752e14b6e2
Max Black <maxblack@trentu.ca>
2025-04-11 21:16:24 -0400

    task 2 number 3 subtask commit

--- src2/VehicleRentalTest.java
+++ src2/VehicleRentalTest.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+
+import java.lang.reflect.Modifier;
 class VehicleRentalTest {
 
 	@Test
@@ -32,10 +35,10 @@ class VehicleRentalTest {
 	public void testRentAndReturnVehicle() {
 	    RentalSystem system = RentalSystem.getInstance();
 
-	    Vehicle car = new Car("Honda", "Accord", 2017, 4);
-	    car.setLicensePlate("CRT360");
+	    Vehicle car = new Car("Aston Martin", "DB5", 1964, 4);
+	    car.setLicensePlate("NBJ117");
 
-	    Customer customer = new Customer(004, "Max Black");
+	    Customer customer = new Customer(007, "James Bond");
 
 	    assertTrue(system.addVehicle(car));
 	    assertTrue(system.addCustomer(customer));
@@ -55,5 +58,15 @@ class VehicleRentalTest {
 
 	    boolean returnAgain = system.returnVehicle(car, customer, LocalDate.now(), 0.0);
 	    assertFalse(returnAgain, "Returning an available vehicle should not work");
+	}
+	@Test
+	public void testSingletonRentalSystem() throws Exception {
+	    Constructor<RentalSystem> constructor = RentalSystem.class.getDeclaredConstructor();
+
+	    int modifiers = constructor.getModifiers();
+	    assertTrue(Modifier.isPrivate(modifiers), "Constructor should be private so it enforces singleton behaviour.");
+
+	    RentalSystem instance = RentalSystem.getInstance();
+	    assertNotNull(instance, "getInstance() should be returning a non-null RentalSystem instance.");
 	}
 }
